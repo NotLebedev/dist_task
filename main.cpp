aaf6e7e53ee04e0470d767c018f2e669fac01eaf
NotLebedev <artem-itf@yandex.ru>
2022-12-10 16:02:50 +0300

    Add stub for client and servers

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <utility>
 #include <toml++/toml.h>
+#include <mpi.h>
 
 enum CommandType {
     CommandRead,
@@ -88,18 +89,71 @@ private:
     std::vector<std::unique_ptr<Command>> command_sequence;
 };
 
-int main(int argc, char **argv) {
-    JobSequence sequence(argv[1]);
+class Client {
+public:
+    explicit Client(const std::string &filename) : jobSequence(filename) {}
 
+    void run();
+private:
+    JobSequence jobSequence;
+};
+
+void Client::run() {
     std::cout << "Files distributed before start:\n";
-    for (const auto &file: sequence.getInitialFiles())
+    for (const auto &file: jobSequence.getInitialFiles())
         std::cout << "\t" << file.first << " = \"" << file.second << "\"\n";
 
     std::cout << "Operations to be performed:\n";
-    for (const auto &operation: sequence.getCommandSequence())
+    for (const auto &operation: jobSequence.getCommandSequence())
         std::cout << "\t" << operation->describe() << "\n";
-
     std::cout << std::endl;
+}
 
+class Server {
+public:
+    Server() : files() {}
+    void run();
+private:
+    class File {
+    public:
+        File() : content() {}
+
+        [[nodiscard]] const std::string &getContent() const {
+            return content;
+        }
+
+        [[nodiscard]] uint64_t getVersion() const {
+            return version;
+        }
+
+        void setContent(const std::string &content_) {
+            content = content_;
+            version++;
+        }
+
+    private:
+        std::string content;
+        uint64_t version = 0;
+    };
+
+    std::map<std::string, File> files;
+};
+
+void Server::run() {
+
+}
+
+int main(int argc, char **argv) {
+    MPI_Init(&argc,&argv);
+    int mpi_proc_id;
+    MPI_Comm_rank(MPI_COMM_WORLD, &mpi_proc_id);
+
+    if (mpi_proc_id == 0)
+        Client(argv[1]).run();
+    else
+        Server().run();
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Finalize();
     return 0;
 }
